b6699675e81bffde8c35e79801b28e99b0431242
semionov-nicolae-216468498 <nicolae@my.yorku.ca>
2020-01-13 12:22:04 -0500

    hi4

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		System.out.println("Hello Wort");
 =======
 		System.out.println("Hello Word");
-		System.out.println("Hello Worddd");
+		System.out.println("Hello Worddd!!!!!!!!");
 		System.out.println("Hello Worddd");
 >>>>>>> branch 'master' of https://github.com/AlAgilly/EECS2311
 	}
