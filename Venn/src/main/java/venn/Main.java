f2ae3bc0ce06bb672598cdfaa64b28f78524f486
Daoud Ali <ali200@my.yorku.ca>
2020-01-13 12:41:03 -0500

    hh

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
+<<<<<<< HEAD
 		System.out.println("Hello Wort");
+=======
 		System.out.println("Hello Word");
-		
+		System.out.println("Hello Worddd!!!!!!!!");
+		System.out.println("Hello Worhhhddd");
+>>>>>>> branch 'master' of https://github.com/AlAgilly/EECS2311
 	}
 
 }
