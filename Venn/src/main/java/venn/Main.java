524961a00fb044422e0a09ea2c0b3aa78b2bba5d
semionov-nicolae-216468498 <nicolae@my.yorku.ca>
2020-01-20 12:12:06 -0500

    merge

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Word");
 		System.out.println("Hello Worddd!!!!!12345345!!!");
-		System.out.println("Hello Worhhhddd");
 	}
 
 }
