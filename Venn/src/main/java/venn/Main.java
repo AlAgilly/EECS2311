e9c5a322be9c7d9cc2414e916a7553bee121cc27
AlAgilly <alagilly@my.yorku.ca>
2020-01-20 12:08:38 -0500

    >:((

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -3,17 +3,9 @@ package venn;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word!");
-<<<<<<< HEAD
 		System.out.println("Hello Word");
-<<<<<<< HEAD
-=======
-
-=======
 		System.out.println("Hello Worddd!!!!!12345345!!!");
 		System.out.println("Hello Worhhhddd");
->>>>>>> branch 'master' of https://github.com/AlAgilly/EECS2311
->>>>>>> refs/remotes/origin/nicolae
 	}
 
 }
