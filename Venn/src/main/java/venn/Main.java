31ef2e518f16cb5de2a95672a0a608a6d67db500
AlAgilly <alagilly@my.yorku.ca>
2020-01-20 13:15:10 -0500

    :)

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -5,17 +5,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(">:(" + "B) ");
-=======
-
-	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("He");
->>>>>>> refs/remotes/origin/nicolae
-=======
 		System.out.println("He");
 		System.out.println("Hehe");
 		
->>>>>>> refs/remotes/origin/nicolae
-	}
+  	}
 	
 }
